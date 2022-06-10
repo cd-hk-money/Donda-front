8265b98c03c74e3206c61f032018ec421a0ec545
codma <ruddud1123@naver.com>
2022-06-10 21:07:34 +0900

    get user bookmark

--- src/store/UserStore.ts
+++ src/store/UserStore.ts
@@ -1,7 +1,8 @@
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 import { IUserAccount } from "@/models/user";
+import { IInterestGroup } from "@/models/interest"
+import InterestStore from './InterestStore'
 import axios from 'axios'
-
 
 @Module({namespaced: true})
 export default class UserStore extends VuexModule {
@@ -11,7 +12,8 @@ export default class UserStore extends VuexModule {
     
     try {
       const res = await axios.post('/api', payload)  
-      console.log(res)          
+      const userBookmarked = [...new Set(res.data.map((interest: IInterestGroup) => interest.item).flat())]
+      InterestStore.state.bookmarked = userBookmarked            
     } catch (e) {
       console.log(e)
     }
